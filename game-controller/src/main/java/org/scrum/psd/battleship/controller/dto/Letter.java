90ca11126fa0d7c1839a70aff9c27aff0d305f58
TeamB3-GP <TeamB3@GaryPedretti.com>
2022-05-17 20:14:45 +0000

    Add validation function for enum validation

--- game-controller/src/main/java/org/scrum/psd/battleship/controller/dto/Letter.java
+++ game-controller/src/main/java/org/scrum/psd/battleship/controller/dto/Letter.java
@@ -3,3 +3,12 @@ package org.scrum.psd.battleship.controller.dto;
 public enum Letter {
     A, B, C, D, E, F, G, H;
 }
+
+public static boolean letterContains(String input) {
+    for (Letter l : Letter.values()) {
+        if (l.name().equals(input)) {
+            return true;
+        }
+    }
+    return false;
+}
